7dbb8999ce1c3e31f19b86615311ebc21ad1c164
sarthak-2001 <sarthakbrahma2001@gmail.com>
2020-11-04 10:57:47 +0530

    first

---
+++ .history/app_20201104105542.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 5005;
+
+app.get('/hello',(req,res)=>{
+    res.send()
+})
---
+++ .history/app_20201104105614.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 5005;
+
+app.get('/hello',(req,res)=>{
+    res.send('Hello people');
+})
+
+app.listen(port,()=>console.log('running on ');
+)
